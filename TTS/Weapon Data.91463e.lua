df5c175d55ab467a5bc6607091093437ced797a7
Avi-Karalis <averkkaralis@gmail.com>
2024-06-28 12:20:16 +0300

    fixed backup weapon data

--- TTS/Weapon Data.91463e.lua
+++ TTS/Weapon Data.91463e.lua
@@ -30,7 +30,7 @@ end
         specialAbilities = {},
         damageMultiplier = 1,
         shortRange = {range = 6, mWModifier = 0, Damage = 12},
-        longRange = {range = 18, mWModifier = -3, Damage = 1},
+        longRange = {range = 18, mWModifier = -3, Damage = 11},
         critFail = 20,
         dynamicCC = false
     }
@@ -384,10 +384,11 @@ end
     }
     delivererBattleblade={
         Name = "Deliverer Battleblade",
-        cCModifier =1,
-        cCDamage = 3,
+        cCModifier =-1,
+        cCDamage = 9,
         specialAbilities = {
-            MultiStrike = 2
+            Reach = 2,
+            Thrust = 2
         },
         damageMultiplier = 1,
         critFail = 20,
@@ -395,11 +396,10 @@ end
     }
     cyberClaws={
         Name = "Cyber Claws",
-        cCModifier = -1,
-        cCDamage = 9,
+        cCModifier = 1,
+        cCDamage = 3,
         specialAbilities = {
-            Thrust = 2,
-            Reach = 2
+            MultiStrike = 2,
         },
         damageMultiplier = 1,
         critFail = 20,
